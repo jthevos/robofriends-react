0d22ae59f11b50b7c86710aa10863f6b9ade0a9f
John G Thevos <admin@mindcette.com>
2019-06-26 17:55:29 -0400

    redux additions

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { searchRobots, requestRobots } from './reducers';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import 'tachyons';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />,
+const logger = createLogger();
+const rootReducer =
+  combineReducers({
+    searchRobots,
+    requestRobots,
+  });
+
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger));
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
   document.getElementById('root')
 );
+
+serviceWorker.unregister();
